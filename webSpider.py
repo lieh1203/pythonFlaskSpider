3b4f2970cb7b7c8e2576a8f71b4cabdc5c8b0fb1
LAPTOP-3HBRAV9M\LiEnHua <lieh1203@163.com>
2018-10-19 16:24:57 +0800

    feat：获取汇率数据接口修改 - 参数忽略大小写

--- webSpider.py
+++ webSpider.py
@@ -50,7 +50,7 @@ dictConfig({'version': 1,
 # > flask run
 # 修改环境变量，修改数据库连接
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-fundKey = config.config[os.getenv('FLASK_CONFIG') or 'default'].fundKey
+
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
@@ -93,56 +93,53 @@ def spiderExchangeRateQuotation(mechanismCode):  # 'ICBC'
 # 获取汇率数据，根据issuingTime查询
 @app.route('/exchangerate/getexchangeratequotationlist', methods=['get'])
 def getexchangeratequotationlist():
-    # app.logger.info(u'getexchangeratequotationlist')
-    # pageindex = request.args.get('pageindex')
-    # pagecount = request.args.get('pagecount')
-    issuingBeginTime = request.args.get('issuingBeginTime')
-    issuingEndTime = request.args.get('issuingEndTime')
+    dicts = request.args.to_dict()
+    issuingBeginTime = None
+    issuingEndTime = None
+    orgcode = None  # 机构code
+    for d in dicts:  # 忽略大小写
+        if 'issuingbeginTime'.lower() == d.lower():
+            issuingBeginTime = dicts.get(d)
+        if 'issuingendTime'.lower() == d.lower():
+            issuingEndTime = dicts.get(d)
+        if 'orgcode'.lower() == d.lower():
+            orgcode = dicts.get(d)
+
     data = None
-    # if pageindex is None:
-    #     pageindex = 1
-    # else:
-    #     pageindex = int(pageindex)
-    # if pagecount is None:
-    #     pagecount = 10
-    # else:
-    #     pagecount = int(pagecount)
     param = []
-    # d=datetime.datetime.strptime(str,"%Y-%m-%d")+datetime.timedelta(days=1)
+
+    if orgcode:
+        param.append(ExchangeRateQuotation.mechanismCode == orgcode)
+
     if issuingBeginTime:
-        # print(db.cast(issuingBeginTime, db.Date))
         # param.append(db.cast(ExchangeRateQuotation.issuingTime, db.Date) >= db.cast(issuingBeginTime, db.Date))
         param.append(ExchangeRateQuotation.issuingTime >= datetime.datetime.strptime(issuingBeginTime, "%Y-%m-%d"))
+
     if issuingEndTime:
         # param.append(db.cast(ExchangeRateQuotation.issuingTime, db.Date) < db.cast(issuingEndTime, db.Date))
         param.append(ExchangeRateQuotation.issuingTime < datetime.datetime.strptime(issuingEndTime,
                                                                                     "%Y-%m-%d") + datetime.timedelta(
             days=1))
-    print(param)
-    # data = ExchangeRateQuotation.query.filter(*param)
-    # db.func.max(ExchangeRateQuotation.acquisitionTime)
+
     items = []
-    # 如果不输入参数，返回最后一次可能有时分秒的差别）导入(acquisitionTime)的那批数据
+    # 如果不输入参数，返回最后一次（可能有时分秒的差别）导入(acquisitionTime)的那批数据
     maxAcquisitionTime = db.session.query(db.func.max(ExchangeRateQuotation.acquisitionTime)).filter(*param).first()
     if maxAcquisitionTime[0]:
-        data = ExchangeRateQuotation.query.filter(ExchangeRateQuotation.acquisitionTime == maxAcquisitionTime[0])
-        print(maxAcquisitionTime[0])
+        param.append(ExchangeRateQuotation.acquisitionTime == maxAcquisitionTime[0])
+
+    data = ExchangeRateQuotation.query.filter(*param)
     if data:
         items = data.all()
-    # totalCount = ExchangeRateQuotation.query.count()
-    # dataPaged = data.paginate(page=pageindex, per_page=pagecount, error_out=False)
-    # print(dataPaged)
-    # r = ResultDtos.PagedResultDto(totalCount=totalCount, items=dataPaged.items)
-    r = ResultDtos.NonPagedResultDto(items=items)
-    # app.logger.info(param)
-    # print(r.data)
 
+    r = ResultDtos.NonPagedResultDto(items=items)
     return r.data
 
-#导入基金数据，批处理调用
+
+# 导入基金数据，批处理调用
 @app.route('/funds/importfunds')
 def importfunds():
     http = urllib3.PoolManager()
+    fundKey = config.config[os.getenv('FLASK_CONFIG') or 'default'].fundKey
     url = 'https://mptest.fofinvesting.com/wxapi/third/fundlist?key={0}'.format(fundKey)
     app.logger.info(url)
     r = http.request('get', url)
@@ -154,6 +151,7 @@ def importfunds():
     else:
         if dataJson.get('amount') and dataJson.get('amount') > 0:
             funds = dataJson.get('funds')
+            now_time = datetime.datetime.now()
             for f in funds:
                 code = f.get('code')
                 title = f.get('title')
@@ -162,22 +160,37 @@ def importfunds():
                 unitNav = f.get('unitNav')
                 navDate = f.get('navDate')
                 subCategory = f.get('subCategory')
-                acquisitionTime = datetime.datetime.now()
                 inserted_funds.append(Fund(code=code, title=title, category=category, annualizedRate=annualizedRate,
                                            unitNav=unitNav, navDate=navDate, subCategory=subCategory,
-                                           acquisitionTime=acquisitionTime,mechanismCode='channel')) #况客基金
+                                           acquisitionTime=now_time, mechanismCode='channel'))  # 况客基金
     db.session.add_all(inserted_funds)
     db.session.commit()
     db.session.close()
     return 'ok'
 
 
+# 获取基金数据列表
 @app.route('/funds')
 def getfoudListByPaged():
-    pageindex = request.args.get('pageindex')
-    pagecount = request.args.get('pagecount')
-    acquisitionTime = request.args.get('acquisitionTime')
-    code = request.args.get('code')
+    dicts = request.args.to_dict()
+    pageindex = None
+    pagecount = None
+    acquisitionTime = None
+    code = None
+    orgcode = None  # 机构code
+
+    for d in dicts:  # 忽略大小写
+        if 'pageindex'.lower() == d.lower():
+            pageindex = dicts.get(d)
+        if 'pagecount'.lower() == d.lower():
+            pagecount = dicts.get(d)
+        if 'acquisitionTime'.lower() == d.lower():
+            acquisitionTime = dicts.get(d)
+        if 'code'.lower() == d.lower():
+            code = dicts.get(d)
+        if 'orgcode'.lower() == d.lower():
+            orgcode = dicts.get(d)
+
     data = None
     if pageindex is None:
         pageindex = 1
@@ -187,19 +200,23 @@ def getfoudListByPaged():
         pagecount = 10
     else:
         pagecount = int(pagecount)
+
+    # 拼接查询条件参数数组
     params = []
 
     if code:
         params.append(Fund.code == code)
 
-    if acquisitionTime:
-        params.append(db.cast(Fund.acquisitionTime ,db.Date) == db.cast(acquisitionTime ,db.Date))
-        #params.append(Fund.acquisitionTime == datetime.datetime.strptime(acquisitionTime, "%Y-%m-%d"))
-    else:  # 默认最后一天的行情，批量插入时，可能会有时分秒的差异，所以要以date格式查询，如果一天中导入多次，会查询出重复数据
-        acquisitionTime = db.session.query(db.func.max(Fund.acquisitionTime)).first()
-        if acquisitionTime[0]:
-            params.append(db.cast(Fund.acquisitionTime ,db.Date) == db.cast(acquisitionTime[0], db.Date))
-            #params.append(Fund.acquisitionTime == acquisitionTime[0])
+    if orgcode:
+        params.append(Fund.mechanismCode == orgcode)
+
+    if acquisitionTime: #比较日期格式，不匹配时分秒格式
+        params.append(db.cast(Fund.acquisitionTime, db.Date) == db.cast(acquisitionTime, db.Date))
+
+    # 默认最后一天的行情
+    max_acquisitionTime = db.session.query(db.func.max(Fund.acquisitionTime)).filter(*params).first()
+    if max_acquisitionTime[0]:
+        params.append(Fund.acquisitionTime == max_acquisitionTime[0])
 
     data = Fund.query.filter(*params)
     totalCount = data.count()
@@ -216,5 +233,5 @@ def after_request(response):
 
 
 if __name__ == '__main__':
-    app.run()
-    #manager.run()
+    #app.run()
+    manager.run()
